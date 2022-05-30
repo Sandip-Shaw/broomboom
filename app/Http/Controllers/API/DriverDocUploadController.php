4237c6e9fcb79d4f3e2b7d870fdb72726297bb26
Sandip Shaw <sandip.alsoltech.astw001@gmail.com>
2022-05-30 18:04:06 +0530

    getUserBranch

--- app/Http/Controllers/API/DriverDocUploadController.php
+++ app/Http/Controllers/API/DriverDocUploadController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\DriverDoc;
+use App\Models\RiderDetail;
+
+use File;
 use Image;
 
 class DriverDocUploadController extends Controller
@@ -105,4 +108,88 @@ class DriverDocUploadController extends Controller
 
         }
     }
+
+
+
+   //////////////rider_details\\\\\\\\\\\\
+    public function riderDetails(Request $request)
+    {
+        $driver=new RiderDetail;
+        $x=$request->request;
+        $driver->name=$request->user()->name;
+       // $driver->vehical_type=$request->user()->vehical_type;
+
+        $data=$request->toArray();
+        
+      //  $driver->name=$data['name'];
+        $driver->mobile=$data['mobile'];
+        $driver->vehical_type=$data['vehical_type'];
+
+        $driver->license_validity=$data['license_validity'];
+        $driver->vehical_no=$data['vehical_no'];
+        $driver->driver_license=$data['driver_license'];
+       
+        #dl_file_front part
+            $file=$request->file('driver_image');
+            $filename='driverImage-'.$file->getClientOriginalName();
+            // $extension=$file->getClientOriginalExtension();
+            $destinationPath = public_path('images/driverDoc/driverImage');
+            $file->move($destinationPath,$filename);
+            $driver->driver_image=$filename;
+
+            if($driver->save()){
+                $success['message'] = "Driver Details Added Successfull";
+                $success['ack'] = 1;
+                return response()->json($success, '400');
+                }
+            else{
+                $success['message'] = "Something went wrong, Please try again";
+                 $success['ack'] = 0;
+                 return response()->json($success, '200');
+        
+                }
+
+     }
+
+
+
+     public function riderDetailsUpdate(Request $request,$id)
+     {
+        $driver= RiderDetail::find($id);
+       // dd($driver);
+        $driver->mobile=$request->mobile;
+        $driver->vehical_type=$request->vehical_type;
+        $driver->license_validity=$request->license_validity;
+        $driver->vehical_no=$request->vehical_no;
+        $driver->driver_license=$request->driver_license;
+       // dd($driver);
+        if($request->hasFile('driver_image')){
+            if($driver->driver_image){
+                $old_path= public_path('images/driverDoc/driverImage/'.$driver->driver_image);
+                if(File::exists($old_path)){
+                    File::delete($old_path);
+                }
+            }
+
+            $filename='driverImage-'.$file->getClientOriginalName();
+            // $extension=$file->getClientOriginalExtension();
+            $destinationPath = public_path('images/driverDoc/driverImage');
+            $file->move($destinationPath,$filename);
+            $driver->driver_image=$filename;
+
+
+            if($driver->update()){
+                $success['message'] = "Driver Details updated Successfull";
+                $success['ack'] = 1;
+                return response()->json($success, '400');
+                }
+            else{
+                $success['message'] = "Something went wrong, Please try again";
+                 $success['ack'] = 0;
+                 return response()->json($success, '200');
+        
+                }
+
+        }
+     }
 }
